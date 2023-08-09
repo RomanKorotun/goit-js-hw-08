4cb270f0ad7e18c9946d67988a25eb63b62edc10
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-08-09 12:01:20 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,35 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+//     description: 'Hokkaido Flower',
+//   }
+
+const galleryList = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+  const markup = arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+    )
+    .join('');
+  galleryList.insertAdjacentHTML('beforeend', markup);
+}
+createMarkup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
