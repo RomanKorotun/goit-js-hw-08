19ed0b4b2965e49c9d581f6564ce9b075f1c119c
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-08-11 12:40:02 +0300

    update Task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,12 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-console.log(player);
 
 player.on('timeupdate', throttle(handlerTime, 1000));
 
 function handlerTime(data) {
   const time = data.seconds;
-  console.log(time);
+
   localStorage.setItem('videoplayer-current-time', time);
 }
 
